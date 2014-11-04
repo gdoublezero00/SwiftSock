1578d6624487ea7e0a8c51b09e8d0ba29ffa30d4
Jun Izumida <jun-izumida@agc.com>
2014-11-05 08:29:29 +0900

    Initial Commit

---
+++ SwiftSock/SwiftSock.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSock.h
+//  SwiftSock
+//
+//  Created by tornado on 11/5/14.
+//  Copyright (c) 2014 jp.co.agel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSock.
+FOUNDATION_EXPORT double SwiftSockVersionNumber;
+
+//! Project version string for SwiftSock.
+FOUNDATION_EXPORT const unsigned char SwiftSockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSock/PublicHeader.h>
+
+
---
+++ SwiftSockTests/SwiftSockTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftSockTests.swift
+//  SwiftSockTests
+//
+//  Created by tornado on 11/5/14.
+//  Copyright (c) 2014 jp.co.agel. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftSockTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
